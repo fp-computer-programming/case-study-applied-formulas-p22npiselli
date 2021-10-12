d79ec08e7b8e4b4b989ec7532be24c512010dcbd
p22npiselli <p22npiselli@fairfieldprep.org>
2021-10-12 10:20:49 -0400

    case study 1-1 & 1-2

---
+++ cs1-1.py
@@ -0,0 +1,14 @@
+# Author: Nolan (AMDG) 10/12/2021
+
+x_one = int(input("Input x1 point: "))
+y_one = int(input("Input y1 point: "))
+x_two = int(input("Input x2 point: "))
+y_two = int(input("Input y2 point: "))
+
+cord_1 = (x_two - x_one) ** 2
+cord_2 = (y_two - y_one) ** 2
+
+d = cord_1 + cord_2
+
+distance = d ** (1/2)
+print(distance)
---
+++ cs1-2.py
@@ -0,0 +1,5 @@
+# Author: Nolan (AMDG) 10/12/2021
+
+p = int(input("Enter the principal investment:  "))
+r = int(input("Enter the annual intrest rate:  "))
+n = int(input("Enter the the number of times that interest is compounded per unit  :  "))
